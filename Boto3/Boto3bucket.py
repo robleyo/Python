050d0c5ab6f4ff03f73e5d0658f9c9b7a20070b3
Robleyo <youssouf2262@gmail.com>
2023-02-17 01:41:53 +0000

    Created S3 Bucket with Boto3

---
+++ Boto3/Boto3bucket.py
@@ -0,0 +1,10 @@
+import boto3
+
+AWS_REGION = "us-west-2"
+resource = boto3.resource("s3", region_name=AWS_REGION)
+bucket_name = "monbucket2231"
+location = {'LocationConstraint': AWS_REGION}
+bucket = resource.create_bucket(
+    Bucket=bucket_name,
+    CreateBucketConfiguration=location)
+print("S3 bucket created")
