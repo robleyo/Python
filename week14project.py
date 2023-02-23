2fa397095b989e0da655c11081810567e0c8fd35
Y Robleh <yrobleh@Ys-MacBook-Air.local>
2023-02-22 19:16:52 -0800

    stopping Ec2 instances

--- week14project.py
+++ week14project.py
@@ -1,20 +1,28 @@
 import boto3
 
-# EC2 pointing to specific region
-ec2_client = boto3.client('ec2', region_name='us-west-2')
+# Set the region 
+region='us-west-2'
 
-# all running EC2 instance Dev tag Environment will be listed 
+#set tag
+environment_tag = 'DEV'
+
+# creation EC2 client object
+ec2_client = boto3.client('ec2', region_name=region)
+
+# finds running instances that match environment_tag  
 instances = ec2_client.describe_instances(Filters=[
- {'Name': 'tag:Environment', 'Values': ['Dev']},
- {'Name': 'instance-state-name', 'Values': ['running']}
+    {'Name': 'tag:Environment', 'Values': [environment_tag]},
+    {'Name': 'instance-state-name', 'Values': ['running']}
 ])
 
-# Extract the instance IDs from the filtered results and store them in a list
+# list instances ids 
 instance_ids = [instance['InstanceId'] for reservation in instances['Reservations'] for instance in reservation['Instances']]
 
 # If there are running instances with the Environment: Dev tag, stop them
 if len(instance_ids) > 0:
- ec2_client.stop_instances(InstanceIds=instance_ids)
- print(f'Successfully stopped {len(instance_ids)} instances: {instance_ids}')
+    ec2_client.stop_instances(InstanceIds=instance_ids)
+    print(f'{len(instance_ids)} {environment_tag} instances stopped')
+    for i in instance_ids:
+        print(i)
 else:
- print('All instances stopped')
+    print(f'no running {environment_tag} instances')
